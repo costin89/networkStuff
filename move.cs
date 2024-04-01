3bc6d1958e51cf22363c00619117ef35d41586e5
Alexander Ast <mailtoast@googlemail.com>
2024-04-01 13:36:51 +0200

    Update move.cs

--- move.cs
+++ move.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishNet.Object;
 
-public class Move : MonoBehaviour
+public class Move : NetworkBehaviour
 {
     [SerializeField] private float speed;
 
     private void Update()
     {
+        if (!IsOwner) return;
+
         Vector3 mInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 move = mInput * speed * Time.deltaTime;
+
         transform.position += move; // Aktualisiere die Position, indem du die Bewegung hinzufügst
     }
 }
